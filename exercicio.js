f233e583b1a7efe5b67f60f52414638f961a047d
Gabriel Guaitanele <gabrieljmym13@gmail.com>
2024-06-04 15:55:43 -0300

    feat: add console log result as comments

--- exercicio.js
+++ exercicio.js
@@ -49,3 +49,8 @@ const student = new Student('João', 15, Gender.M, ['gym', 'videogames']);
 person.about();
 teacher.about();
 student.about();
+
+// node exercicio.js 
+// Person: Gabriel, Male - 22yrs.
+// Teacher: Maria, Female - 42yrs, Subjects: [history,science]
+// Student: João, Male - 15yrs, Hobbies: [gym,videogames]
